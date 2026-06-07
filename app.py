22677c9866440d8d981bc2868ad743d1c5d33154
Aribah <aribahhh.ar@gmail.com>
2026-06-07 22:08:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "joblib", "scikit-learn", "numpy"])
+
 import streamlit as st
 import joblib
 import numpy as np
